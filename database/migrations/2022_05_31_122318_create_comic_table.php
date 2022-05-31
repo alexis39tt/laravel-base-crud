1b76b6062793f3a1bde9bb705197666259031397
alexis39tt <alessandro.fonzi@gmail.com>
2022-05-31 14:26:03 +0200

    created controller, model and migration

---
+++ database/migrations/2022_05_31_122318_create_comic_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comic', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('thumb');
+            $table->float('price');
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comic');
+    }
+}
